507b378df4448fd13a9c106d6b1928a53300a127
Manas <manas@zopsmart.com>
2019-12-15 14:22:02 +0530

    Add action from the buttonTap of WebView

--- InstaClone/Features/Box-Animation/BoxViewController.swift
+++ InstaClone/Features/Box-Animation/BoxViewController.swift
@@ -7,72 +7,37 @@
 //
 
 import UIKit
+import WebKit
 
+/// This controller has a WKWebView on which intercepting an action and allow notification is used
 class BoxViewController: UIViewController {
 
     
-    @IBOutlet weak var topFlap: UIView!
-    @IBOutlet weak var leftFlap: UIView!
-    @IBOutlet weak var rightFlap: UIView!
-    @IBOutlet weak var bottomFlap: UIView!
-    @IBOutlet weak var animateBtn: UIButton!
-        
-    @IBOutlet weak var topFlapHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var bottomView: UIView!
+    
+    let htmlCode: String = "<html><body><a class=\"yourButton\" href=\"inapp://capture\">Change Color</a></body></html>"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupWebView()
+        webView.navigationDelegate = self
     }
-
-    var isOpen = true
     
-    @IBAction func toggleAnimation(_ sender: UIButton) {
+    func setupWebView() {
+        webView.loadHTMLString(htmlCode, baseURL: nil)
+    }
+}
 
-        let foldedTopFlapFrame = topFlap.frame.offsetBy(dx: 0, dy: 64)
-        let foldedRightFlapFrame = rightFlap.frame.offsetBy(dx: -64, dy: 0)
-        let foldedLeftFlapFrame = leftFlap.frame.offsetBy(dx: 64, dy: 0)
-        let foldedBottomFlapFrame = bottomFlap.frame.offsetBy(dx: 0, dy: -64)
-        
-//        UIView.transition(with: topFlap, duration: 2.0, options: .transitionFlipFromBottom, animations: {
-//            self.topFlap.frame = foldedTopFlapFrame
-//            self.topFlap.backgroundColor = .black
-//        }, completion: nil)
-//
-//        UIView.transition(with: rightFlap, duration: 2.0, options: .transitionFlipFromLeft, animations: {
-//            self.rightFlap.frame = foldedRightFlapFrame
-//            self.rightFlap.backgroundColor = .cyan
-//        }, completion: nil)
-//
-//        UIView.transition(with: bottomFlap, duration: 2.0, options: .transitionFlipFromTop, animations: {
-//            self.bottomFlap.frame = foldedBottomFlapFrame
-//            self.bottomFlap.backgroundColor = .blue
-//        }, completion: nil)
-//
-//        UIView.transition(with: leftFlap, duration: 2.0, options: .transitionFlipFromRight, animations: {
-//            self.leftFlap.frame = foldedLeftFlapFrame
-//            self.leftFlap.backgroundColor = .green
-//        }, completion: nil)
-        
-        
-//        if isOpen {
-//            isOpen = false
-//            leftFlap.backgroundColor = .black
-//            UIView.transition(with: leftFlap, duration: 1.0, options: .transitionFlipFromRight, animations: nil, completion: nil)
-//        } else {
-//            isOpen = true
-//            leftFlap.backgroundColor = .cyan
-//            UIView.transition(with: leftFlap, duration: 1.0, options: .transitionFlipFromRight, animations: nil, completion: nil)
-//        }
-    
-//        UIView.transition(from: topFlap, to: bottomFlap, duration: 2.0, options: [.transitionCurlUp, .showHideTransitionViews, .repeat, .autoreverse], completion: nil)
-        
-        
-        UIView.animate(withDuration: 2.0) {
-            self.leftFlap.layer.transform = CATransform3DMakeRotation(.pi, 0.0, 1.0, 0.0)
+extension BoxViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let urlStr = navigationAction.request.url {
+            if urlStr.scheme == "inapp" {
+                if urlStr.host == "capture" {
+                    bottomView.backgroundColor = .red
+                }
+            }
         }
-        
+        decisionHandler(.allow)
     }
-    
-    
 }
