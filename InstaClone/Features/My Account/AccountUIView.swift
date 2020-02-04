dada3102e1d2e3efe2968c07deab6689177f2563
Fury-2K <manas.agrawal990@gmail.com>
2020-02-04 15:47:57 +0530

    Fix ios13 error

--- InstaClone/Features/My Account/AccountUIView.swift
+++ InstaClone/Features/My Account/AccountUIView.swift
@@ -8,12 +8,14 @@
 
 import SwiftUI
 
+@available(iOS 13.0, *)
 struct AccountUIView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+@available(iOS 13.0, *)
 struct AccountUIView_Previews: PreviewProvider {
     static var previews: some View {
         AccountUIView()
--- InstaClone/Features/My Account/UpperAccountUIView.swift
+++ InstaClone/Features/My Account/UpperAccountUIView.swift
@@ -8,12 +8,14 @@
 
 import SwiftUI
 
+@available(iOS 13.0, *)
 struct UpperAccountUIView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+@available(iOS 13.0, *)
 struct UpperAccountUIView_Previews: PreviewProvider {
     static var previews: some View {
         UpperAccountUIView()
