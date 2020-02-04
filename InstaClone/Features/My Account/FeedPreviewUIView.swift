a605b5a0aa847d87bd1dc3e5e74cb3dcbeff033f
Fury-2K <manas.agrawal990@gmail.com>
2020-02-04 15:40:34 +0530

    Fix GoogleFirebase plist file path

---
+++ InstaClone/Features/My Account/FeedPreviewUIView.swift
@@ -0,0 +1,21 @@
+//
+//  FeedPreviewUIView.swift
+//  InstaClone
+//
+//  Created by Manas Aggarwal on 04/02/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import SwiftUI
+
+struct FeedPreviewUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedPreviewUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedPreviewUIView()
+    }
+}
