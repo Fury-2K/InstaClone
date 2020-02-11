75bb88573abb28e0f93a3905f4cef7cb1bc9ed8c
Fury-2K <manas.agrawal990@gmail.com>
2020-02-11 16:25:01 +0530

    Add Loader

---
+++ InstaClone/Extensions/UIViewController.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewController.swift
+//  InstaClone
+//
+//  Created by Manas Aggarwal on 11/02/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import Foundation
+import  UIKit
+
+extension UIViewController {
+    
+    func showLoadingAnimation() {
+        view.showLoadingAnimation()
+    }
+
+    func hideLoadingAnimation() {
+        view.hideLoadingAnimation()
+    }
+    
+}
