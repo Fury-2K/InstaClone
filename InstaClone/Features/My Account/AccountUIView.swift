a299f9dcd08aaf1cc845c66ab5fd6465cb9e13c1
Manas Aggarwal <manas.agrawal990@gmail.com>
2020-02-11 12:37:55 +0530

    Revert "Revert 9 swift ui integration"

---
+++ InstaClone/Features/My Account/AccountUIView.swift
@@ -0,0 +1,21 @@
+//
+//  AccountUIView.swift
+//  InstaClone
+//
+//  Created by Manas Aggarwal on 03/02/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import SwiftUI
+
+struct AccountUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountUIView()
+    }
+}
---
+++ InstaClone/Features/My Account/UpperAccountUIView.swift
@@ -0,0 +1,21 @@
+//
+//  UpperAccountUIView.swift
+//  InstaClone
+//
+//  Created by Manas Aggarwal on 04/02/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import SwiftUI
+
+struct UpperAccountUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UpperAccountUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        UpperAccountUIView()
+    }
+}
